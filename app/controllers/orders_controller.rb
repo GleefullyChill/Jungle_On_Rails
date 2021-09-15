69529d96c184484b2b62c6799f5c1fbc612af044
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-15 15:57:26 -0600

    add a basic AR call to LineItem

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,6 +10,7 @@ class OrdersController < ApplicationController
 
     if order.valid?
       redirect_to order, notice: 'Your Order has been placed.'
+      ordered_cart
       empty_cart!
     else
       redirect_to cart_path, flash: { error: order.errors.full_messages.first }
@@ -25,6 +26,16 @@ class OrdersController < ApplicationController
     # empty hash means no products in cart :)
     update_cart({})
   end
+  def line_items
+    @line_items ||= LineItem.find_by order_id: params[:id]
+  end
+  helper_method :line_items
+
+  def ordered_items
+    @ordered_items = line_items
+    #@ordered_items ||= Product.where(id: line_items[product_id]).map {|product| { product:product, quantity:line_items[quantity], total_price_cents:line_items[total_price_cents] } }
+  end
+  helper_method :ordered_items
 
   def perform_stripe_charge
     Stripe::Charge.create(
