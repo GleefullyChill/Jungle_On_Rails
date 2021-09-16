4d1689fdef72a8314f59744c9ee62dfe43ba3866
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-16 12:59:07 -0600

    add route, view, and controller logic for login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,11 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+  #resources :sessions, only: [:new, :create, :destroy]
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
