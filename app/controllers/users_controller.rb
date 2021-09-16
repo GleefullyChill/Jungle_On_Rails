a3c0883d83765d3897696a7e8799ce3f299339bf
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-16 13:23:34 -0600

    add functional signup, login, logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       session[:user_id] = user.id
-      redirect_to '/'
+      redirect_to :root, notice: 'Account created successfully!'
     else
-      redirect_to '/signup'
+      flash[:error] = 'An error occured!'
+      render 'new'
     end
   end
 
