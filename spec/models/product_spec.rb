baf71054f88c21a4952a50535d0dfb6dbbcc6f90
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-20 20:26:35 -0600

    add a missing test to product_spec, add user_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    it "is valid with a name, price, quantity, and a proper category" do
+      @name = "Sloppy Joes"
+      @price = 500.25
+      @quantity = 5
+      @category = Category.create(:name => "Carpentry")
+      product = Product.new(name: @name, price: @price, quantity: @quantity, category: @category)
+    end
     it "is not valid without a name" do
       product = Product.new(name: nil)
       expect(product).to_not be_valid
