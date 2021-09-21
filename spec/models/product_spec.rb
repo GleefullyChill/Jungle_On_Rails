65f640e39abc89baefdf945b0bbec87504ae58ce
GledfullyChill <stephan.lofstrom@gmail.com>
2021-09-20 21:36:06 -0600

    add fixes to product_spec, add code to validations for user_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -7,23 +7,39 @@ RSpec.describe Product, type: :model do
       @price = 500.25
       @quantity = 5
       @category = Category.create(:name => "Carpentry")
-      product = Product.new(name: @name, price: @price, quantity: @quantity, category: @category)
+      @product = Product.new(name: @name, price: @price, quantity: @quantity, category: @category)
+      expect(@product).to be_valid
     end
     it "is not valid without a name" do
-      product = Product.new(name: nil)
-      expect(product).to_not be_valid
+      @name = nil
+      @price = 500.25
+      @quantity = 5
+      @category = Category.create(:name => "Carpentry")
+      @product = Product.new(name: @name, price: @price, quantity: @quantity, category: @category)
+      expect(@product).to_not be_valid
     end
     it "is not valid without a price" do
-      product = Product.new(price: nil)
-      expect(product).to_not be_valid
+      @name = "Sloppy Joes"
+      @price = nil
+      @quantity = 5
+      @category = Category.create(:name => "Carpentry")
+      @product = Product.new(name: @name, price: @price, quantity: @quantity, category: @category)
+      expect(@product).to_not be_valid
     end
     it "is not valid without a quantity" do
-      product = Product.new(quantity: nil)
-      expect(product).to_not be_valid
+      @name = "Sloppy Joes"
+      @price = 500.25
+      @quantity = nil
+      @category = Category.create(:name => "Carpentry")
+      @product = Product.new(name: @name, price: @price, quantity: @quantity, category: @category)
+      expect(@product).to_not be_valid
     end
     it "is not valid without a category" do
-      product = Product.new(category: nil)
-      expect(product).to_not be_valid
+      @name = "Sloppy Joes"
+      @price = 500.25
+      @quantity = 5
+      @product = Product.new(name: @name, price: @price, quantity: @quantity, category: @category)
+      expect(@product).to_not be_valid
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,200 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    it "is valid when all fields are correctly given" do
+      @first_name = 'Stacy'
+      @last_name = 'Mathews'
+      @email = 'SandM@bdsm.ca'
+      @password = 'saturdaynight'
+      @password_confirmation = 'saturdaynight'
+
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      expect(@user).to be_valid
+    end
+
+    it "is not valid without a first_name" do
+      @first_name = nil
+      @last_name = 'Mathews'
+      @email = 'SandM@bdsm.ca'
+      @password = 'saturdaynight'
+      @password_confirmation = 'saturdaynight'
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a last_name" do
+      @first_name = 'Stacy'
+      @last_name = nil
+      @email = 'SandM@bdsm.ca'
+      @password = 'saturdaynight'
+      @password_confirmation = 'saturdaynight'
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a email" do
+      @first_name = 'Stacy'
+      @last_name = 'Mathews'
+      @email = nil
+      @password = 'saturdaynight'
+      @password_confirmation = 'saturdaynight'
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a password" do
+      @first_name = 'Stacy'
+      @last_name = 'Mathews'
+      @email = 'SandM@bdsm.ca'
+      @password = nil
+      @password_confirmation = 'saturdaynight'
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid if the passwords and password confirmation are different" do
+      @first_name = 'Stacy'
+      @last_name = 'Mathews'
+      @email = 'SandM@bdsm.ca'
+      @password = 'saturdaynight'
+      @password_confirmation = nil
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a password of 8 characters" do
+      @first_name = 'Stacy'
+      @last_name = 'Mathews'
+      @email = 'SandM@bdsm.ca'
+      @password = 'night'
+      @password_confirmation = 'night'
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      expect(@user).to_not be_valid
+    end
+
+    it "is not valid without a password confirmation" do
+      @first_name = 'Stacy'
+      @last_name = 'Mathews'
+      @email = 'SandM@bdsm.ca'
+      @password = 'saturdaynight'
+      @password_confirmation = 'saturdaynight'
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      @user.valid?
+      expect(@user.errors.messages[:password_confirmation]).to include("doesn't match Password")
+    end
+
+    it "is not valid if the email is already in use, ignoring mixed case" do
+      @user_first = User.create(
+        :first_name => 'Sandy',
+        :last_name => 'Mathews',
+        :email => 'SandM@bdsm.ca',
+        :password => 'saturdaynight',
+        :password_confirmation => 'saturdaynight'
+      )
+      @user_first.save
+      
+      @first_name = 'Stacy'
+      @last_name = 'Mathews'
+      @email = 'SANDM@BDSM.CA'
+      @password = 'saturdaynight'
+      @password_confirmation = 'saturdaynight'
+      @user = User.new(
+      first_name: @first_name,
+      last_name: @last_name,
+      email: @email,
+      password: @password,
+      password_confirmation: @password_confirmation
+      )
+      expect(@user).to_not be_valid
+    end
+  end
+
+  describe '.authenticate_with_credentials' do
+    it "successfully logs in when provided proper information" do
+      @user1 = User.create(
+        :first_name => 'Stacy',
+        :last_name => 'Mathews',
+        :email => 'SandM@bdsm.ca',
+        :password => 'saturdaynight',
+        :password_confirmation => 'saturdaynight'
+      )
+    end
+    it "successfully logs in when given mixed case email" do
+      @user = User.create(
+        :first_name => 'Stacy',
+        :last_name => 'Mathews',
+        :email => 'SandM@bdsm.ca',
+        :password => 'saturdaynight',
+        :password_confirmation => 'saturdaynight'
+      )
+    end
+    it "successfully logs in when user adds space on either side of email" do
+      @user = User.create(
+        :first_name => 'Stacy',
+        :last_name => 'Mathews',
+        :email => 'SandM@bdsm.ca',
+        :password => 'saturdaynight',
+        :password_confirmation => 'saturdaynight'
+      )
+    end
+    it "successfully  prevents logging in when provided wrong information" do
+      @user = User.create(
+        :first_name => 'Stacy',
+        :last_name => 'Mathews',
+        :email => 'SandM@bdsm.ca',
+        :password => 'saturdaynight',
+        :password_confirmation => 'saturdaynight'
+      )
+    end
+  end
 end
